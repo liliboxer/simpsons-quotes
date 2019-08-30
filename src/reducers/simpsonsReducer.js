c7d9b483ddbb68b4b9cd699930d2870b2c6f0694
Lili Boxer <ninaliboxer@gmail.com>
2019-08-30 14:34:42 -0700

    changed quote state from array to string

--- src/reducers/simpsonsReducer.js
+++ src/reducers/simpsonsReducer.js
@@ -1,11 +1,15 @@
+import { FETCH_QUOTE } from '../actions/simpsonsActions';
+
 const initialState = {
-  quotes: [],
+  quote: '',
   loading: false,
   error: null
 };
 
 export default function simpsonsReducer(state = initialState, action) {
   switch(action.type) {
+    case FETCH_QUOTE:
+      return { ...state, quote: action.payload };
     default: 
       return state;
   }
