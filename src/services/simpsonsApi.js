ed40d6974af81b15c25347cd9680b7495879c2fe
Lili Boxer <ninaliboxer@gmail.com>
2019-08-30 14:45:45 -0700

    fetch getting multiple quotes because it looks nicer. also massaging data

--- src/services/simpsonsApi.js
+++ src/services/simpsonsApi.js
@@ -1,7 +1,14 @@
-export const getQuote = () => {
-  return fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+export const getQuote = (count = 5) => {
+  return fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=${count}`)
     .then(res => {
       if(!res.ok) throw 'Unable to fetch quote';
       return res.json();
+    })
+    .then(quotes => {
+      return quotes.map(quote => ({
+        characterName: quote.character,
+        quote: quote.quote,
+        characterImage: quote.image
+      }));
     });
 };
