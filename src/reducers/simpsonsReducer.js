72a05b542477eb1cdc7f9856b32f57c6cc370a59
Lili Boxer <ninaliboxer@gmail.com>
2019-08-30 11:45:14 -0700

    setup for reducers

---
+++ src/reducers/simpsonsReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  quotes: [],
+  loading: false,
+  error: null
+};
+
+export default function simpsonsReducer(state = initialState, action) {
+  switch(action.type) {
+    default: 
+      return state;
+  }
+}
